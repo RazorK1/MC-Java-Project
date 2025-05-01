577eb60914cd16deafbb3c40a641775cf3b3d1eb
Kyle Harris <87076483+RazorK1@users.noreply.github.com>
2025-05-01 11:14:32 -0400

    Create IInventory.java

---
+++ IInventory.java
@@ -0,0 +1,30 @@
+package net.minecraft.server;
+
+public interface IInventory {
+
+    int getSize();
+
+    ItemStack getItem(int i);
+
+    ItemStack splitStack(int i, int j);
+
+    ItemStack splitWithoutUpdate(int i);
+
+    void setItem(int i, ItemStack itemstack);
+
+    String getInventoryName();
+
+    boolean k_();
+
+    int getMaxStackSize();
+
+    void update();
+
+    boolean a(EntityHuman entityhuman);
+
+    void startOpen();
+
+    void closeContainer();
+
+    boolean b(int i, ItemStack itemstack);
+}
