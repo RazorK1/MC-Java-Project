c9cc42de91130da7b8d094ec462e6bd82d8cf326
Kyle Harris <87076483+RazorK1@users.noreply.github.com>
2025-05-02 13:12:57 -0400

    Update UltraEntityCreeper.java

--- UltraEntityCreeper.java
+++ UltraEntityCreeper.java
@@ -7,7 +7,7 @@ public class UltraEntityCreeper extends EntityCreeper {
     private int maxFuseTicks = 4; /////////// Explosion timer here (Original value = 30)
     private int explosionRadius = 30; //////// Increase blast radius here (Original Value = 3)  
 
-    public void die(DamageSource damagesource) {
+    public void die(DamageSource damagesource) { ////////// (This isn't needed)
         super.die(damagesource);
         if (damagesource.getEntity() instanceof EntitySkeleton) {
             int i = Item.getId(Items.RECORD_1);
@@ -16,6 +16,10 @@ public class UltraEntityCreeper extends EntityCreeper {
 
             this.a(Item.getById(k), 1);
         }
+    }
+
+    protected Item getLoot() {
+        return Items.DIAMOND_PICKAXE; /////////// Set loot here (Original Value = SULPHUR)
     }
 
 }
