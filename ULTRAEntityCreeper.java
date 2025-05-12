cbc735fd564f8e88c57eba312c8d1273751cef14
Kyle Harris <87076483+RazorK1@users.noreply.github.com>
2025-05-12 17:46:48 -0400

    !!!!!!!!!!!!OBSOLETE!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!OBSOLETE!!!!!!!!!!!!!!!!!!

--- ULTRAEntityCreeper.java
+++ ULTRAEntityCreeper.java
@@ -5,7 +5,7 @@ public class ULTRAEntityCreeper extends EntityMonster {
     private int bp;
     private int fuseTicks;
     private int maxFuseTicks = 3; ////////// Explosion timer here (Original value = 30)
-    private int explosionRadius = 30; //////// Increase blast radius here (Original Value = 3)  
+    private int explosionRadius = 30; /////// Increase blast radius here (Original Value = 3)  
 
     public EntityCreeper(World world) {
         super(world);
