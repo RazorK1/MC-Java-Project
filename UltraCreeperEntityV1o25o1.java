eafe43b837063ec560c106cdfbef7b74d3b163e2
Kyle Harris <87076483+RazorK1@users.noreply.github.com>
2025-05-12 14:28:31 -0400

    Update UltraCreeperEntityV1o25o1.java
    Added Commands N stuff

--- UltraCreeperEntityV1o25o1.java
+++ UltraCreeperEntityV1o25o1.java
@@ -15,6 +15,11 @@ public class UltraCreeperEntity extends CreeperEntity {
 
     public UltraCreeperEntity(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
+
+        //super(type, world);
+		//xpReward = 0;
+		//setNoAi(false);
+        
     }
 
     @Override
@@ -42,4 +47,64 @@ public class UltraCreeperEntity extends CreeperEntity {
         Item droppedItem = lootTable.get(random.nextInt(lootTable.size()));
         this.dropItem(droppedItem);
     }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////// Command thy here!
+    public final AnimationState animationState0 = new AnimationState();
+
+	@Override
+	protected void registerGoals() {
+		super.registerGoals();
+
+		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false) {
+
+			@Override
+			protected boolean canPerformAttack(LivingEntity entity) {
+				return this.isTimeToAttack() && this.mob.distanceToSqr(entity) < (this.mob.getBbWidth() * this.mob.getBbWidth() + entity.getBbWidth()) && this.mob.getSensing().hasLineOfSight(entity);
+			}
+
+		});
+		this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1));
+		this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
+		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
+		this.goalSelector.addGoal(5, new FloatGoal(this));
+
+	}
+
+	@Override
+	public SoundEvent getHurtSound(DamageSource ds) {
+		return BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.generic.hurt"));
+	}
+
+	@Override
+	public SoundEvent getDeathSound() {
+		return BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse("entity.generic.death"));
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		if (this.level().isClientSide()) {
+			this.animationState0.animateWhen(true, this.tickCount);
+		}
+	}
+
+	public static void init(RegisterSpawnPlacementsEvent event) {
+		event.register(DemoModModEntities.ULTRACREEPER.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+				(entityType, world, reason, pos, random) -> (world.getDifficulty() != Difficulty.PEACEFUL && Monster.isDarkEnoughToSpawn(world, pos, random) && Mob.checkMobSpawnRules(entityType, world, reason, pos, random)),
+				RegisterSpawnPlacementsEvent.Operation.REPLACE);
+	}
+
+	public static AttributeSupplier.Builder createAttributes() {
+		AttributeSupplier.Builder builder = Mob.createMobAttributes();
+		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
+		builder = builder.add(Attributes.MAX_HEALTH, 10);
+		builder = builder.add(Attributes.ARMOR, 0);
+		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
+		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
+
+		builder = builder.add(Attributes.STEP_HEIGHT, 0.6);
+
+		return builder;
+	}
+
 }
