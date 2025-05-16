426ed9ca1f550e1946cefa62628e4b4dae20235a
Kyle Harris <87076483+RazorK1@users.noreply.github.com>
2025-05-16 10:37:27 -0400

    Update UltraCreeperEntityV1o25o1.java

--- UltraCreeperEntityV1o25o1.java
+++ UltraCreeperEntityV1o25o1.java
@@ -96,11 +96,11 @@ public class UltraCreeperEntity extends CreeperEntity {
 
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
-		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
+		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.2);
 		builder = builder.add(Attributes.MAX_HEALTH, 10);
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
-		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
+		builder = builder.add(Attributes.FOLLOW_RANGE, 12);
 
 		builder = builder.add(Attributes.STEP_HEIGHT, 0.6);
 
